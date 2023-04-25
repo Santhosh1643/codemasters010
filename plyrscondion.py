2f4f309c5ddb8263b68769472b6d823eca878f93
codemasters010 <131702603+codemasters010@users.noreply.github.com>
2023-04-25 20:37:53 +0530

    Update plyrscondion.py

--- plyrscondion.py
+++ plyrscondion.py
@@ -12,23 +12,31 @@ def players():
         player_2_postion=player_2.def draw() 
 
         if  player_1_postion == player_2_postion:
-            player_1_postion=0
+            player_1_postion-=player_1_postion
             
             return player_1
             print("position has already filled")
         elif player_1_postion == player_1_postion:
-             player_1_postion=0
+             player_1_postion-=player_1_postion
              return player_1
              print("position has already filled")
         elif player_2_postion == player_2_postion:
-             player_2_postion=0
+             player_2_postion-=player_2_postion
              return player_2
              print("position has already filled")
         elif player_2_postion == player_1_postion:
-             player_2_postion=0
+             player_2_postion-=player_2_postion
              return player_2
              print("position has already filled")
             rounds_for_player_1+=player_1
             print("player1 have entering",round,"round")
             rounds_for_player_2+=player_2
             print("player2 have entering",round,"round")
+            if player_1< len(x[3]):
+                print("player_1 had tried all his/her chance")
+            elif player_2<len(x[3]):
+                print("player_2 had tried all his/her chance")
+            else :
+                return player()
+                
+            
