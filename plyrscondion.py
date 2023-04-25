b149043cb009e19f07fa4fbd22f9ab862324e64e
codemasters010 <131702603+codemasters010@users.noreply.github.com>
2023-04-25 18:45:43 +0530

    Create plyrscondion.py

---
+++ plyrscondion.py
@@ -0,0 +1,25 @@
+def players():
+    player_1=int(input("enter a position to apply x OR o:"))
+    while player_1==x:
+        print("you had determined position👍")
+        input("enter x or o :")
+        player_1_postion=player_1.def draw()
+            
+    player_2=int(input("enter a position to apply x OR o:"))
+    while player_2==x:
+        print("you had determined position👍")
+        input("enter x or o :")
+        player_2_postion=player_2.def draw() 
+
+        if  player_1_postion == player_2_postion:
+            return player_1
+            print("position has already filled")
+        elif player_1_postion == player_1_postion:
+             return player_1
+             print("position has already filled")
+        elif player_2_postion == player_2_postion:
+             return player_2
+             print("position has already filled")
+        elif player_2_postion == player_1_postion:
+             return player_2
+             print("position has already filled")
